a0fc37c00a34d31e481eccb9ec9d97880024567e
Dany Renaudier <dani11_21@hotmail.com>
2022-02-15 23:40:07 -0300

    Update general

--- database/config.js
+++ database/config.js
@@ -4,7 +4,7 @@ const dbConnection=async()=>{
 
     try {
         
-        await mongoose.connect(process.env.URL,{
+        await mongoose.connect(process.env.MONGO_DB,{
             useNewUrlParser: true,
             useUnifiedTopology: true,
 
